531a5e3740594ddb4b0be0dd2bba476a25e3d0d4
EstimaSahuma <estimaetilenoglicol@gmail.com>
2023-08-03 10:40:40 +0100

    fix: init git

---
+++ src/test/java/com/corebase/codebase/CodeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corebase.codebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
